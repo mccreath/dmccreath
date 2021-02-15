7f08288d0d7be9669a0f29ff300e30385c46b81a
David McCreath <david@davids-mac-mini.local>
2021-02-15 21:39:03 +0000

    switching things to PHP, adding some includes, restructuring home page

---
+++ includes/site-footer.php
@@ -0,0 +1,14 @@
+<footer>
+	<div class="contact h-card">
+		<p class="p-name">
+			<a href="http://dmccreath.org/" class="u-url">
+				<span class="p-given-name">David</span> <span class="p-family-name">McCreath</span>
+			</a>
+		</p>
+		<p><a href="mailto:mccreath@gmail.com" rel="me"  class="u-email">mccreath@gmail.com</a></p>
+		<p><a href="https://twitter.com/mccreath/" rel="me">@mccreath on Twitter</a></p>
+		<p><a href="https://github.com/mccreath/" rel="me">mccreath on GitHub</a></p>
+		<!-- <p><a href="https://alpha.app.net/mccreath" rel="me">mccreath on App.net</a></p> -->
+		<p>(907) 227-0966</p>
+	</div>
+</footer>
---
+++ includes/site-header.php
@@ -0,0 +1,4 @@
+<header>
+	<h1>David McCreath</h1>
+	<h2>internet &amp; art &amp; music</h2>
+</header>

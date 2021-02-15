dd9b88ab1e6889598093aa4004a91df8b9ae0825
David McCreath <david@davids-mac-mini.local>
2021-02-15 03:36:12 +0000

    adding taxanomie

---
+++ taxanomie/index.php
@@ -0,0 +1,259 @@
+<?php
+	$offset = 3600 * 24;
+	$expire = "Expires: ".gmdate("D, d M Y H:i:s", time() + $offset)." GMT";
+	Header($expire);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>taxanomie. a small collection of small collages.</title>
+	<!--[if lt IE 9]>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js" type="text/javascript"></script>
+	<![endif]-->
+	<link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+	<link rel="manifest" href="favicon/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+	<link href="css/lightbox.css" type="text/css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Crimson+Text:400,700" rel="stylesheet" type="text/css">
+	
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:url" content="http://dmccreath.org/taxanomie/">
+	<meta name="twitter:title" content="taxanomie. ">
+	<meta name="twitter:description" content="a small collection of small collages.">
+	<meta name="twitter:site" content="@mccreath">
+	<meta name="twitter:domain" content="dmccreath.org">
+	<meta name="twitter:image:src" content="http://dmccreath.org/taxanomie/meta2.jpg">
+	<meta name="twitter:image:width" content="627">
+	<meta name="twitter:image:height" content="328">
+	<meta name="twitter:creator" content="@mccreath">
+
+	<meta property="og:title" content="taxanomie.">
+	<meta property="og:site_name" content="taxanomie">
+	<meta property="og:url" content="http://dmccreath.org/taxanomie/">
+	<meta property="fb:app_id" content="289697811187011">
+	<meta content="a small collection of small collages." property="og:description" />
+	<meta content="http://dmccreath.org/taxanomie/meta.jpg" property="og:image" />
+	<meta content="image/jpeg" property="og:image:type" />
+	<meta content="627" property="og:image:width" />
+	<meta content="328" property="og:image:height" />
+	
+	<style>
+		body { background:#DFDDDE;font-family: 'Crimson Text', serif; }
+		a { color:#0000cc; }
+		h1 { text-align:center;margin:80px 0 100px; }
+		h2 { margin:0; }
+		#footer,.hint { font-size:12px;text-align:center;margin-bottom:50px; }
+		#whats-all-this-then { width:90%;max-width:580px;margin:0 auto 100px;padding:30px 0; }
+		#wrapper { overflow:hidden;width:90%;max-width:1050px;margin:0 auto;text-align:center;padding-bottom:200px; }
+		.item { float:left;width:200px;height:500px;text-align:center;font-weight:bold;margin:5px; }
+		.item a:hover { cursor:zoom-in; }
+		.item img { display:block;width:100%; }
+		.sources { text-align:left;position:absolute;bottom:0;left:0;background:#fff;width:600px;z-index:10;display:none;padding:10px; }
+		.sources ul { font-family:monospace;font-weight:normal;font-size:12px;margin-left:0;padding-left:0; }
+		#source-links { width:90%;max-width:780px;padding:20px;background:#fff;margin:30px auto;overflow:hidden;display:none; }
+		#source-links ol { list-style-type:upper-roman;font-family:Courier,monospace;font-size:75%; }
+	</style>
+</head>
+<body>
+<h1>taxanomie</h1>
+
+<p class="hint">click to view larger version</p>
+
+<div id="wrapper">
+	<div class="item">
+		<a href="01-I-h.jpg" onclick="return false;"><img src="sm/01-I.jpg" data-jslghtbx="01-I-h.jpg"></a>
+		<p>I.</p>
+	</div>
+	<div class="item"><a href="02-II-h.jpg" onclick="return false;"><img src="sm/02-II.jpg" data-jslghtbx="02-II-h.jpg"></a><p>II.</p></div>
+	<div class="item"><a href="03-III.jpg" onclick="return false;"><img src="sm/03-III.jpg" data-jslghtbx="03-III.jpg"></a><p>III.</p></div>
+	<div class="item"><a href="04-IV.jpg" onclick="return false;"><img src="sm/04-IV.jpg" data-jslghtbx="04-IV.jpg"></a><p>IV.</p></div>
+	<div class="item"><a href="05-V.jpg" onclick="return false;"><img src="sm/05-V.jpg" data-jslghtbx="05-V.jpg"></a><p>V.</p></div>
+	<div class="item"><a href="06-VI.jpg" onclick="return false;"><img src="sm/06-VI.jpg" data-jslghtbx="06-VI.jpg"></a><p>VI.</p></div>
+	<div class="item"><a href="07-VII.jpg" onclick="return false;"><img src="sm/07-VII.jpg" data-jslghtbx="07-VII.jpg"></a><p>VII.</p></div>
+	<div class="item"><a href="08-VIII.jpg" onclick="return false;"><img src="sm/08-VIII.jpg" data-jslghtbx="08-VIII.jpg"></a><p>VIII.</p></div>
+	<div class="item"><a href="09-IX.jpg" onclick="return false;"><img src="sm/09-IX.jpg" data-jslghtbx="09-IX.jpg"></a><p>IX.</p></div>
+	<div class="item"><a href="10-X.jpg" onclick="return false;"><img src="sm/10-X.jpg" data-jslghtbx="10-X.jpg"></a><p>X.</p></div>
+	<div class="item"><a href="11-XI.jpg" onclick="return false;"><img src="sm/11-XI.jpg" data-jslghtbx="11-XI.jpg"></a><p>XI.</p></div>
+	<div class="item"><a href="12-XII.jpg" onclick="return false;"><img src="sm/12-XII.jpg" data-jslghtbx="12-XII.jpg"></a><p>XII.</p></div>
+	<div class="item"><a href="13-XIII.jpg" onclick="return false;"><img src="sm/13-XIII.jpg" data-jslghtbx="13-XIII.jpg"></a><p>XIII.</p></div>
+	<div class="item"><a href="14-XIV.jpg" onclick="return false;"><img src="sm/14-XIV.jpg" data-jslghtbx="14-XIV.jpg"></a><p>XIV.</p></div>
+	<div class="item"><a href="15-XV.jpg" onclick="return false;"><img src="sm/15-XV.jpg" data-jslghtbx="15-XV.jpg"></a><p>XV.</p></div>
+	<div class="item"><a href="16-XVI.jpg" onclick="return false;"><img src="sm/16-XVI.jpg" data-jslghtbx="16-XVI.jpg"></a><p>XVI.</p></div>
+	<div class="item"><a href="17-XVII.jpg" onclick="return false;"><img src="sm/17-XVII.jpg" data-jslghtbx="17-XVII.jpg"></a><p>XVII.</p></div>
+	<div class="item"><a href="18-XVIII.jpg" onclick="return false;"><img src="sm/18-XVIII.jpg" data-jslghtbx="18-XVIII.jpg"></a><p>XVIII.</p></div>
+	<div class="item"><a href="19-XIX.jpg" onclick="return false;"><img src="sm/19-XIX.jpg" data-jslghtbx="19-XIX.jpg"></a><p>XIX.</p></div>
+	<div class="item"><a href="20-XX.jpg" onclick="return false;"><img src="sm/20-XX.jpg" data-jslghtbx="20-XX.jpg"></a><p>XX.</p></div>
+	<div class="item"><a href="21-XXI.jpg" onclick="return false;"><img src="sm/21-XXI.jpg" data-jslghtbx="21-XXI.jpg"></a><p>XXI.</p></div>
+	<div class="item"><a href="22-XXII.jpg" onclick="return false;"><img src="sm/22-XXII.jpg" data-jslghtbx="22-XXII.jpg"></a><p>XXII.</p></div>
+	<div class="item"><a href="23-XXIII.jpg" onclick="return false;"><img src="sm/23-XXIII.jpg" data-jslghtbx="23-XXIII.jpg"></a><p>XXIII.</p></div>
+	<div class="item"><a href="24-XXIV-h.jpg" onclick="return false;"><img src="sm/24-XXIV.jpg" data-jslghtbx="24-XXIV-h.jpg"></a><p>XXIV.</p></div>
+	<div class="item"><a href="25-XXV.jpg" onclick="return false;"><img src="sm/25-XXV.jpg" data-jslghtbx="25-XXV.jpg"></a><p>XXV.</p></div>
+	<div class="item"><a href="26-XXVI.jpg" onclick="return false;"><img src="sm/26-XXVI.jpg" data-jslghtbx="26-XXVI.jpg"></a><p>XXVI.</p></div>
+	<div class="item"><a href="27-XXVII-h.jpg" onclick="return false;"><img src="sm/27-XXVII.jpg" data-jslghtbx="27-XXVII-h.jpg"></a><p>XXVII.</p></div>
+	<div class="item"><a href="28-XXVIII-h.jpg" onclick="return false;"><img src="sm/28-XXVIII.jpg" data-jslghtbx="28-XXVIII-h.jpg"></a><p>XXVIII.</p></div>
+	<div class="item"><a href="29-XIX.jpg" onclick="return false;"><img src="sm/29-XXIX.jpg" data-jslghtbx="29-XXIX.jpg"></a><p>XXIX.</p></div>
+	<div class="item"><a href="30-XXX.jpg" onclick="return false;"><img src="sm/30-XXX.jpg" data-jslghtbx="30-XXX.jpg"></a><p>XXX.</p></div>
+</div>
+<div id="footer">
+	<p>A small collection of small collages. <a href="#whats-all-this-then" onclick="manageWatt();">What’s all this then?</a></p> 
+	<p>&copy; 2016 David McCreath <a href="mailto:mccreath@gmail.com">mccreath@gmail.com</a> | <a href="http://twitter.com/mccreath/">@mccreath</a></p>
+	<p><a href="/">dmccreath.org</a></p>
+	<!--p><a href="">Source links</a>.</p-->
+</div>
+<div id="whats-all-this-then">
+	<p>The company where I work (Slack) has an anniversary party every year around the day that our product came out of its private beta in February, 2014. (This is in lieu of a holiday party, which I think is a pretty great idea. I mean, who needs another stupid holiday party? And how nice to have a lovely party with some friends in the middle of February!)</p>
+	<p>Anyway, this year (2016), the party was declared to have a 1920s theme, with recommendations of "Downton Abbey" and "Great Gatsby" for attire. I'm not much on Downton or Gatsby, but the '20s was an amazing time for art. Specifically, it was the time when the various experiments of Dada came to their zenith, and then the Dadaists transitioned to Surrealism. There is a <em>lot</em> to be discussed about both Dada and Surrealism in the context of 2016, but the artists of both groups produced a large percentage of my favorite art.</p>
+	<p>So I started playing around with Dada/Surrealist collages. Then I thought it would be nice to make them small enough to hand out at the party. While at the office supply store getting paper, I noticed these hang tags and thought they'd be the perfect size and format.</p> 
+	<p>I ended up making thirty of them, and took them to the party, but it never really seemed like the right time to hand them to people. There was too much else going on and it was dark so they would have been hard to see, and I didn't want to have to shout over the DJ to explain what I was handing to someone.</p>
+	<p>So now they are here, fed back into the web where I sourced the images in the first place.</p> 
+	<h2>Colophon</h2>
+	<p>All of the images came from Google searches. Where I can, I will provide sources.</p>
+	<p>The lightbox for the images is from <a href="https://github.com/felixhagspiel/jsOnlyLightbox">https://github.com/felixhagspiel/jsOnlyLightbox</a>.</p>
+	<p>The text is set in <a href="https://www.google.com/fonts/specimen/Crimson+Text">Crimson Text</a> from Google Fonts.</p>
+	<p><a href="#footer" onclick="manageWhat();">Close this</a>.</p>
+</div>
+<div id="source-links">
+	<h2>Sources</h2>
+	<ol>
+		<li>
+			<a href="http://devilsartisan.ca/dingbats_section_brazil.html">http://devilsartisan.ca/dingbats_section_brazil.html</a><br>
+			<a href="http://www.123rf.com/photo_13770852_stock-photo.html">http://www.123rf.com/photo_13770852_stock-photo.html</a>
+		</li>
+		<li>
+			<a href="http://porcupinesquill.ca/blog/?p=1387">http://porcupinesquill.ca/blog/?p=1387</a><br>
+			<a href="http://devilsartisan.ca/dingbats_section_misc.html">http://devilsartisan.ca/dingbats_section_misc.html</a> (cormorant)<br>
+			<a href="http://devilsartisan.ca/dingbats_section_brazil.html">http://devilsartisan.ca/dingbats_section_brazil.html</a> (toucan)
+		</li>
+		<li>
+		http://devilsartisan.ca/dingbats_section_brazil.html (parrot)<br>
+		http://devilsartisan.ca/dingbats_section_misc.html (seated man)
+		</li>
+		<li>
+		http://devilsartisan.ca/dingbats_section_brazil.html (monkey)<br>
+		http://devilsartisan.ca/dingbats_section_misc.html (seated man)
+		</li>
+		<li>
+		http://devilsartisan.ca/dingbats_section_brazil.html (monkey)<br>
+		http://devilsartisan.ca/dingbats_section_misc.html (seated man)
+		</li>
+		<li>
+		http://museumvictoria.com.au/caughtandcoloured/sharks.aspx (men in boat)<br>
+		http://etc.usf.edu/clipart/12200/12270/chkwillwidow_12270.htm (bird)
+		</li>
+		<li>
+		https://commons.wikimedia.org/wiki/File:Engraved_portrait_of_Marie_Josephine_Louise_of_Savoy_-_18th_century.jpg (portrait)<br>
+		https://commons.wikimedia.org/wiki/File:Leonids-1833.jpg (background)<br>
+		https://www.pinterest.com/pin/60306082480356979/ (skull)
+		</li>
+		<li>
+		http://www.hps.cam.ac.uk/starry/armillpoems.html (sphere)<br>
+		http://www.fotosearch.com/CSP697/k6976564/ (octopus)
+		</li>
+		<li>
+		https://www.pinterest.com/pin/60306082480356979/ (skull)<br>
+		http://vintageephemera.blogspot.com/2010/11/engraved-illustration-insect-vivarium.html (vivarium)
+		</li>
+		<li>
+		https://commons.wikimedia.org/wiki/File:Engraved_portrait_of_Marie_Josephine_Louise_of_Savoy_-_18th_century.jpg (face)<br>
+		https://www.pinterest.com/pin/60306082480356979/ (skull)
+		</li>
+	</ol>
+</div>
+<!-- 
+01.	http://devilsartisan.ca/dingbats_section_brazil.html 
+		http://www.123rf.com/photo_13770852_stock-photo.html (3 credits/40 credits, $39)
+02. http://porcupinesquill.ca/blog/?p=1387
+		http://devilsartisan.ca/dingbats_section_misc.html (cormorant)
+		http://devilsartisan.ca/dingbats_section_brazil.html (toucan)
+03. http://devilsartisan.ca/dingbats_section_brazil.html (parrot)
+		http://devilsartisan.ca/dingbats_section_misc.html (seated man)
+04. http://devilsartisan.ca/dingbats_section_brazil.html (monkey)
+		http://devilsartisan.ca/dingbats_section_misc.html (seated man)
+05. http://devilsartisan.ca/dingbats_section_brazil.html (monkey)
+		http://devilsartisan.ca/dingbats_section_misc.html (seated man)
+06. http://museumvictoria.com.au/caughtandcoloured/sharks.aspx (men in boat)
+		http://etc.usf.edu/clipart/12200/12270/chkwillwidow_12270.htm (bird) (http://etc.usf.edu/clipart/info/license)
+07. https://commons.wikimedia.org/wiki/File:Engraved_portrait_of_Marie_Josephine_Louise_of_Savoy_-_18th_century.jpg (portrait)
+		https://commons.wikimedia.org/wiki/File:Leonids-1833.jpg (background)
+		https://www.pinterest.com/pin/60306082480356979/ (skull)
+08. http://www.hps.cam.ac.uk/starry/armillpoems.html (sphere)
+		http://www.fotosearch.com/CSP697/k6976564/ (octopus) ($3.00)
+09. https://www.pinterest.com/pin/60306082480356979/ (skull)
+		http://vintageephemera.blogspot.com/2010/11/engraved-illustration-insect-vivarium.html (vivarium)
+10. https://commons.wikimedia.org/wiki/File:Engraved_portrait_of_Marie_Josephine_Louise_of_Savoy_-_18th_century.jpg (face)
+		https://www.pinterest.com/pin/60306082480356979/ (skull)
+11. http://www.aradergalleries.com/detail.php?id=4133 (claw)
+		
+12.
+13.
+14.
+15.	http://etc.usf.edu/clipart/12200/12270/chkwillwidow_12270.htm (snake & branch) (http://etc.usf.edu/clipart/info/license)
+16.
+17.
+18. https://commons.wikimedia.org/wiki/File:Leonids-1833.jpg (wing background)
+19.
+20.
+21.
+22.
+23.
+24.
+25.
+26.	http://etc.usf.edu/clipart/12200/12270/chkwillwidow_12270.htm (wing) (http://etc.usf.edu/clipart/info/license)
+27.
+28.
+29.
+30. http://devilsartisan.ca/dingbats_section_misc.html (cobra)
+		http://www.hps.cam.ac.uk/starry/armillpoems.html (text panel)
+		
+-->
+<script src="js/lightbox.min.js" type="text/javascript"></script>
+<script>
+	var what = document.getElementById('whats-all-this-then');
+	what.style.display = "none";
+	
+	function manageWhat(){
+		if(what.style.display == "none"){
+			what.style.display = "block";
+		} else {
+			what.style.display = "none";
+		}
+		return false;
+	}
+	
+	
+	var lightbox = new Lightbox();
+	var lightboxOptions = {
+		onload: function(){
+			var cur = document.getElementsByClassName('jslghtbx-next-on-click')[0].getAttribute('src');
+			ga('send', 'event', cur, 'open', 'taxanomie');
+		}
+	}
+	lightbox.load(lightboxOptions);
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-73791-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
